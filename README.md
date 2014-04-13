0950f3533a23e9f6962f4a28ed8a8bb544d5fba7
柯拓 <xiaowei.lxw@alibaba-inc.com>
2014-04-13 19:59:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,123 @@
+#de-heredoc
+Transform "heredoc call" to "string literal" in JavaScript source code before minifications.
+
+## Install
+```bash
+$ npm install de-heredoc
+```
+
+## Usage
+`heredoc` is a function to make multiline strings in JavaScript, there are already some libraries you can use e.g. [heredoc](https://github.com/jden/heredoc) and [multiline](https://github.com/sindresorhus/multiline).
+```bash
+$ cat test.js
+var heredoc = require("heredoc");
+var html = heredoc(function(){/*
+    <div id="test">
+        <span>foo</span>
+        <span>bar</span>
+    </div>
+*/});
+$ node
+> var deHeredoc = require("de-heredoc")
+> deHeredoc("test.js")
+(^C again to quit)
+>
+$ cat test.js
+var html='<div id="test">\n    <span>foo</span>\n    <span>bar</span>\n</div>';
+```
+## Options
+```javascript
+deHeredoc(file[, options])
+```
+`options` has 3 properties, `from` `dependency` `beautify`.
+### from
+Control `file`'s type.
+#### "file" (default)
+`file` is a file path, change this file in place and return `undefined`.
+#### "string"
+`file` is a string of JS source code, return the new source code.
+```javascript
+deHeredoc('var foo = heredoc(function(){/*foo*/})',{
+    from: "string"
+}) // 'var foo="foo";'
+```
+#### "ast"
+`file` is an UglifyJS AST object, return the new AST object.
+```javascript
+var ast = UglifyJS.parse('var foo = heredoc(function(){/*foo*/})')
+var newAst = deHeredoc(ast, {
+    from: "ast"
+})
+newAst.print_to_string() // 'var foo="foo";'
+```
+### dependency
+The `heredoc` library's name. 
+#### "heredoc" (default)
+```javascript
+deHeredoc('var multiline = require("maybesomepath/multiline");' +
+          'var foo = multiline(function(){/*foo*/})',{
+    from: "string",
+    dependency: "multiline"
+}) // 'var foo="foo";'
+```
+###beautify
+Beautify output source code or not.
+#### false (default)
+```javascript
+deHeredoc('if(1+1){var foo=heredoc(function(){/*foo*/})}', {
+    from: "string",
+    beautify: true
+}) 
+/*
+if (1 + 1) {
+    var foo = "foo";
+}
+*/
+```
+## Whitespaces
+You can use parameter name to control whitespaces in output string literals.
+### indent (default)
+Strip the redundant leading whitespaces, and preserve other indents.
+```javascript
+var html = heredoc(function(indent){/*
+    <div id="test">
+        <span>foo</span>
+        <span>bar</span>
+    </div>
+*/});
+```
+will be transformed to
+```javascript
+var html='<div id="test">\n    <span>foo</span>\n    <span>bar</span>\n</div>';
+```
+### raw
+Preserve all whitespaces.
+```javascript
+var html = heredoc(function(raw){/*
+    <div id="test">
+        <span>foo</span>
+        <span>bar</span>
+    </div>
+*/});
+```
+will be transformed to
+```javascript
+var html='    <div id="test">\n        <span>foo</span>\n        <span>bar</span>\n    </div>';
+```
+### oneline
+Remove all leading and trailing whitespaces.
+```javascript
+var html = heredoc(function(oneline){/*
+    <div id="test">
+        <span>foo</span>
+        <span>bar</span>
+    </div>
+*/});
+```
+will be transformed to
+```javascript
+var html='<div id="test"><span>foo</span><span>bar</span></div>';
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,94 @@
+var UglifyJS = require("uglify-js")
+var fs = require("fs")
+
+module.exports = function (code, options) {
+  options = options || {}
+  var from = options.from || "file"
+  var dependency = options.dependency || "heredoc"
+  var treeTransformer = getTreeTransformer(dependency)
+  
+  if(from === "ast") {
+    var ast = code
+    return ast.transform(treeTransformer)
+  }
+  else if(from === "file") {
+    var file = code
+    var code = fs.readFileSync(file, "utf8")
+    fs.writeFileSync(file, getNewCode(), "utf8")
+  }else if(from === "string"){
+    return getNewCode()
+  }
+
+  function getNewCode() {
+    return UglifyJS.parse(code).transform(treeTransformer).print_to_string({
+      beautify: options.beautify
+    })
+  }
+}
+
+function getTreeTransformer(dependency) {
+  return new UglifyJS.TreeTransformer(function (node, descend) {
+    if(node.TYPE === "Call" &&
+      node.args.length === 1 &&
+      node.args[0].TYPE === "Function" &&
+      node.args[0].body.length === 0 &&
+      node.args[0].end &&
+      node.args[0].end.comments_before &&
+      node.args[0].end.comments_before.length === 1 &&
+      node.args[0].end.comments_before[0].type === "comment2"
+    ) {
+      var comment = node.args[0].end.comments_before[0].value
+      comment = comment.replace(/^\s*?\n|\s*\n$/g, "")
+      var arg = node.args[0].argnames[0]
+      var argname = arg ? arg.name : ""
+      if(argname === "raw") {
+        comment = comment
+      } else if(argname === "oneline") {
+        comment = comment.replace(/\s*^\s*|\s*$/mg, "")
+      } else {
+        var shortest = comment.split("\n").map(function (str) {
+          return str.match(/^\s*/)[0].length
+        }).sort(function (a, b) {
+          return a - b
+        })[0]
+        comment = comment.replace(RegExp("^.{" + shortest + "}", "mg"), "")
+      }
+      return new UglifyJS.AST_String({
+        value: comment
+      })
+    }
+    if(dependency) {
+      if(node.TYPE === "Var") {
+        if(node.definitions.length === 1 &&
+          isHeredocDefinition(node.definitions[0])) {
+          return new UglifyJS.AST_EmptyStatement
+        } else {
+          node.definitions = node.definitions.filter(function (definition) {
+            return !isHeredocDefinition(definition)
+          })
+        }
+      }
+    }
+    descend(node, this)
+    return node
+
+    function isHeredocDefinition(definition) {
+      return (definition.name.TYPE === "SymbolVar" &&
+        definition.value.TYPE === "Call" &&
+        definition.value.expression.name === "require" &&
+        definition.value.args.length === 1 &&
+        matchDependency(dependency, definition.value.args[0].value)) ||
+        (definition.name.TYPE === "SymbolVar" &&
+        definition.value.TYPE === "Dot" &&
+        definition.value.TYPE === "Call" &&
+        definition.value.expression.expression.name === "require" &&
+        definition.value.expression.args.length === 1 &&
+        matchDependency(dependency, definition.value.args[0].value))
+    }
+
+    function matchDependency(dependency, value) {
+      return dependency === value ||
+        value.indexOf("/" + dependency) + dependency.length === value.length - 1
+    }
+  })
+}
